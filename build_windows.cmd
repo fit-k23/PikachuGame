dfbbb2bf27664fd013d35146dd86e6e34f57bf5a
Arie <nttispay@gmail.com>
2024-04-18 16:32:00 +0700

    update cmd built script

--- build_windows.cmd
+++ build_windows.cmd
@@ -6,12 +6,15 @@ if %ERRORLEVEL%==0 (
 	set GCC_BIN=g++
 )
 if "%GCC_BIN%"=="" (
-    echo "Cannot find g++ binary."
+    echo Cannot find g++ binary.
     goto END
+) else (
+    echo Found G++ binary
 )
 set CFLAGS="-std=c++17"
 
-set BUILD_FLAGS="-O3"
+@REM set BUILD_FLAGS="-O2"
+set BUILD_FLAGS=""
 
 %GCC_BIN% %CFLAGS% %BUILD_FLAGS% -o pikachu.exe src/main.h src/main.cpp
 if %ERRORLEVEL%==0 (
